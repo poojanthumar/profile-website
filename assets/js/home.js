eb6e6e0988f7b1f13c4daf8fad735cf63c3b57bf
poojanthumar <poojanthumar@gmail.com>
2021-01-10 01:11:02 +0530

    Profile Section ReDone

--- assets/js/home.js
+++ assets/js/home.js
@@ -3,12 +3,15 @@ console.log("Script in");
 
   $("#nav-ed").on('click', function(event) {
     setTimeout(function () {
+      event.preventDefault();
+
         $(window).scrollTop($('#div2').offset().top - screen.height/3);
     }, 0);
   });
   $("#nav-prof").on('click', function(event) {
+      event.preventDefault();
     setTimeout(function () {
-        $(window).scrollTop($('#div3').offset().top - screen.height/3);
+        $(window).scrollTop($('#div3').offset().top - screen.height/3.5);
     }, 0);
   });
   
@@ -27,19 +30,19 @@ document.addEventListener('scroll',function(){
         const dist = $(this)[0].getBoundingClientRect().top - aeroTop;
         if(dist < screen.height/10 && dist > -screen.height/10)
         {
-            $(this).css({"box-shadow": "0px 0px 20px white", "color": "white", "transform": "scaleY(1.1)"});
-            $('.timeline-circle', this).css({"box-shadow": "0px 0px 30px white"});
+            $(this).css({"box-shadow": "0px 0px 20px rgb(39, 206, 24)", "color": "rgb(39, 206, 24)", "transform": "scaleY(1.1)"});
+            $('.timeline-circle', this).css({"box-shadow": "0px 0px 30px rgb(39, 206, 24)"});
         }
         else
         {
-            $(this).css({"box-shadow": "0px 0px 0px white", "color": "black", "transform": "scaleY(1)"});
+            $(this).css({"box-shadow": "0px 0px 0px white", "color": "white", "transform": "scaleY(1)"});
             $('.timeline-circle', this).css({"box-shadow": "0px 0px 0px white"});
 
         }
 
         if(aeroTop < screen.height/(3))
         {
-            $(this).css({"box-shadow": "0px 0px 0px white", "color": "black", "transform": "scaleY(1)"})
+            $(this).css({"box-shadow": "0px 0px 0px white", "color": "white", "transform": "scaleY(1)"})
             opval = (aeroTop * 300 / screen.height ) - 20 ;
             $('#ed-header, .aeroplane').css("opacity", opval + "%");
             $('.timeline-circle', this).css({"box-shadow": "0px 0px 0px white"});
@@ -66,7 +69,7 @@ document.addEventListener('scroll',function(){
         $('.project').each(function(){
             const profTop = $(this)[0].getBoundingClientRect().top;
             const profDown = $(this)[0].getBoundingClientRect().bottom;
-            let profDist = (screen.height/2) - ((profTop + profDown) / 2) + screen.height/15;
+            let profDist = (screen.height/2) - ((profTop + profDown) / 2);
             if(screen.height/10 > profDist && profDist > (-1)*screen.height/10)
             {
                 let scale = Math.abs(profDist*10/screen.height);
@@ -76,23 +79,25 @@ document.addEventListener('scroll',function(){
                 $(this).css({
                     "transform": "scale(" + scale + ")" ,
                     "z-index": "3" , 
-                    "background-image": "linear-gradient(to bottom, rgb(0, 0, 0),rgb(0, 0, 0) 5%, rgba(29, 92, 29),rgb(40, 150, 40))",
-                    "background-repeat": "no-repeat",
-                "background-size": "auto",
-                "background-attachment": "fixed"
+                    "background-color": "black",
+                    "box-shadow": "0px 0px 30px rgb(17, 224, 51)",
+                    "color": "rgb(39, 206, 24)"
             })
 
                 
 
             }
             else{
-                $(this).css({"transform": "scale(1)","z-index": "1" , "background-color":"transparent" , "background": "none"});
+                $(this).css({"transform": "scale(1)","z-index": "1" , "background-color":"transparent", "box-shadow": "0px 0px 0px rgb(17, 224, 51)", "color": "white" });
             }
             
         })
     }
     else{
+        
         $('.project').each(function(){
+
+            $(this).css({"transform": "scale(1)","z-index": "1" , "background-color":"transparent", "box-shadow": "0px 0px 40px black", "color": "white" });
             $(this).hover(function(){
                 $(this).css({"transform": "rotateY(180deg)"});
             },function(){
